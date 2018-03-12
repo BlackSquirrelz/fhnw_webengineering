95072fb25d73555ea945c01edd5e19832569a22c
BlackSquirrelz <tobias.weisskopf.ch@gmail.com>
2018-03-12 09:18:14 +0100

    config

--- config.php
+++ config.php
@@ -5,3 +5,11 @@
  * Date: 3/11/18
  * Time: 14:35
  */
+
+<?php
+   define('DB_SERVER', 'localhost:8888');
+   define('DB_USERNAME', 'admin');
+   define('DB_PASSWORD', '12345678');
+   define('DB_DATABASE', 'database');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
