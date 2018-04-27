ffa31c99000864bae22d61d58faa1440c0357d4b
BlackSquirrelz <tobias.weisskopf.ch@gmail.com>
2018-04-27 10:38:08 +0200

    small changes

--- router/Router.php
+++ router/Router.php
@@ -5,18 +5,14 @@
  * Date: 22.09.2017
  * Time: 10:42
  */
-
 namespace router;
-
 use http\Exception;
 use http\HTTPException;
 use http\HTTPStatusCode;
 use http\HTTPHeader;
-
 class Router
 {
     protected static $routes = [];
-
     public static function init(){
         $protocol = isset($_SERVER['HTTPS'])||(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === "https") ? 'https' : 'http';
         $_SERVER['SERVER_PORT'] === "80" ? $serverPort = "" : $serverPort = ":" . $_SERVER['SERVER_PORT'];
@@ -28,11 +24,9 @@ class Router
             $_SERVER['PATH_INFO'] = "/";
         }
     }
-
     public static function route($method, $path, $routeFunction) {
         self::route_auth($method, $path, null, $routeFunction);
     }
-
     public static function route_auth($method, $path, $authFunction, $routeFunction) {
         if(empty(self::$routes))
             self::init();
@@ -47,7 +41,6 @@ class Router
         }
         self::$routes[$method][$path] = array("authFunction" => $authFunction, "routeFunction" => $routeFunction);
     }
-
     public static function call_route($method, $path) {
         $path = trim(parse_url($path, PHP_URL_PATH), '/');
         $path_pieces = explode('/', $path);
@@ -70,13 +63,10 @@ class Router
         }
         $route["routeFunction"](...$parameters);
     }
-
     public static function errorHeader() {
         HTTPHeader::setStatusHeader(HTTPStatusCode::HTTP_404_NOT_FOUND);
     }
-
     public static function redirect($redirect_path) {
         HTTPHeader::redirect($redirect_path);
     }
-
 }
