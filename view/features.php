4ed54f02e8c9eb242cdc979fa26ab36c86282016
roger <roger.kuratle@students.fhnw.ch>
2018-06-11 00:09:38 +0200

    ....

--- view/features.php
+++ view/features.php
@@ -19,25 +19,26 @@ include 'landingheader.php';
         </div>
         <div class="row">
             <div class="col-md-3">
-                <h4>Feature 1</h4>
+                <h4>Start creating</h4>
                 <p>Create your project</p>
                 <p>Use Timecatchers to create your own projects and keep track of your work</p>
                 <p>Or use it collaborate with your team members on their projects</p>
             </div>
             <div class="col-md-3">
-                <h4>Feature 2</h4>
-                <p>Send Emails directly from the Webapplication</p>
+                <h4>Become efficient</h4>
+                <p>Send Emails and reports directly from the Webapplication</p>
             </div>
             <div class="col-md-3">
-                <h4>Feature 3</h4>
+                <h4>Be safe</h4>
                 <p>If you forgot your password, no worries.</p>
                 <p>Just click "Reset Password" and you receive a link to your email adresss to reset it.</p>
             </div>
             <div class="col-md-3">
-                <h4>Feature 4</h4>
+                <h4>Collaborate</h4>
                 <p>Allocate tasks to team members</p>
             </div>
         </div>
+        <h5>Join us and become a timecatcher today</h5>
     </div>
 </div>
 
