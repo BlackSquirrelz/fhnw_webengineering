29a2640f1fe9e859058adc7c31984c83e70b4e8b
FWitschi <fabian.witschi@students.fhnw.ch>
2018-06-10 23:38:57 +0200

    made some style changes - however, was not able to log in and therefore could no more modify the user's personal page...

---
+++ view/landingheader.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fabian
+ * Date: 10.06.18
+ * Time: 22:15
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Timecatcher</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,600">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Search.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body id="base-div">
+<nav class="navbar navbar-light navbar-expand-md" id="navbar">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="<?php echo $GLOBALS["ROOT_URL"]; ?>/login">
+            <img src="assets/img/timecatcher-icon.png" class="logo">
+            <strong>&nbsp; TimeCatcher</strong>
+        </a>
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active" href="<?php echo $GLOBALS["ROOT_URL"]; ?>/pricing">Pricing</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active" href="<?php echo $GLOBALS["ROOT_URL"]; ?>/features">Features</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active" href="<?php echo $GLOBALS["ROOT_URL"]; ?>/whoweare">Who we are</a>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <a class="nav-link active" href="<?php echo $GLOBALS["ROOT_URL"]; ?>/login">Log in</a>
+                    </li>
+                </ul>
+                <a class="btn btn-primary" role="button" href="<?php echo $GLOBALS["ROOT_URL"]; ?>/register">SIGN UP</a>
+        </div>
+    </div>
+</nav>
---
+++ view/userfooter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fabian
+ * Date: 10.06.18
+ * Time: 22:23
+ */
+?>
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
